0363b5f3d88638bd697e552cc8a098fbe3a57941
Peter Street <PeterBStreet@gmail.com>
2018-01-29 08:38:47 -0700

    Functions

--- snap-calculator.php
+++ snap-calculator.php
@@ -2,6 +2,36 @@
 /**
  * Created by PhpStorm.
  * User: petersdata
+ * @author Peter Street <PeterBStreet@gmail.com
  * Date: 1/29/18
  * Time: 8:10 AM
  */
+
+         function addFunction($num1, $num2) {
+				$sum = $num1 + $num2;
+				echo "Sum of the two numbers is : $sum";
+			}
+
+         addFunction(20, 10);
+
+         function subtractFunction($num1, $num2) {
+         	$difference = $num1 - $num2;
+         	echo "Difference of  the two numbers is : $difference";
+			}
+
+			subtractFunction(20, 10);
+
+         function multiplyFunction($num1, $num2) {
+         	$multiply = $num1 * $num2;
+         	echo "The product of multiplying the two numbers is : $multiply";
+			}
+
+			multiplyFunction(20, 10);
+
+         function divideFunction($num1, $num2) {
+         	$divide = $num1 / $num2;
+         	echo "The product of dividing the two numbers is : $divide";
+			}
+
+			divideFunction(20, 10);
+
